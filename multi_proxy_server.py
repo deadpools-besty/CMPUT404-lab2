a74ce9013e70230ea9d07a80022ac0bfea246f32
Nana Andoh <nanaegyir2000@hotmail.com>
2021-01-20 18:41:56 -0700

    finished i think?

---
+++ multi_proxy_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from multiprocessing.context import Process
+from echo_server import BUFFER_SIZE, HOST, PORT
+import socket, sys
+
+#get host information
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print (f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+def main():
+    
+    host = 'www.google.com'
+    port = 80
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start:
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST, PORT))
+        proxy_start.listen(1)
+        while True:
+            conn, addr = proxy_start.accept()
+            print("Connected by", addr)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
+                print("Connecting to Google")
+                remote_ip = get_remote_ip(host)
+                proxy_end.connect((remote_ip, port))
+                p = Process(target=handle_request, args=(addr, conn, proxy_end))    
+                p.daemon = True
+                p.start()
+                print("Started process", p)
+            conn.close()
+
+def handle_request(addr, conn, proxy_end):
+    print("Connected by", addr)
+
+    full_data = conn.recv(BUFFER_SIZE)
+    conn.sendall(full_data)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
